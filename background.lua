d37e65b2f657a5e54a6ca199f35820eacf7b9616
Matheus Sales <matheus.sales@enjoei.com.br>
2021-12-28 11:02:08 -0300

    Add parallax background

---
+++ background.lua
@@ -0,0 +1,22 @@
+Background = {}
+
+function Background:load()
+  self.background_image = love.graphics.newImage("sprites/background.jpg")
+  self.background_x = 0
+  self.background_spd = 500
+end
+
+function Background:update(dt)
+  self.background_x = self.background_x - self.background_spd * dt
+  local minimum_width = -love.graphics.getWidth()
+
+  if self.background_x < minimum_width then
+      self.background_x = 0
+  end
+end
+
+function Background:draw()
+  love.graphics.draw(self.background_image, self.background_x, 0)
+  love.graphics.draw(self.background_image, self.background_x + self.background_image:getWidth(), 0)
+end
+
