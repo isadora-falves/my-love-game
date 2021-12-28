f788552fff19c314296275961ffad47023ac2943
Isadora Alves <isadora.alves@enjoei.com.br>
2021-12-28 11:08:28 -0300

    add timer

--- main.lua
+++ main.lua
@@ -58,6 +58,9 @@ function love.load()
     -- colocar imagem correta
     enemy.sprite = love.graphics.newImage("sprites/parrot.png")
 
+    remaining_time = 40
+    gameover = false
+
     -- player.image:setFilter("nearest", "nearest")
     -- enemy.image:setFilter("nearest", "nearest")
 
@@ -97,6 +100,14 @@ function love.update(dt)
     if player.x + player.width > game.width then
       player.x = game.width - player.width
     end
+
+    remaining_time = remaining_time - dt
+    print("remaining_time " .. (remaining_time))
+
+    if remaining_time <= 0 then
+        gameover = true
+        -- aqui tem que parar o jogo e fazer o perdeu
+    end
 end
 
 function love.draw()
@@ -104,102 +115,3 @@ function love.draw()
     love.graphics.draw(player.sprite, player.x, player.y)
     love.graphics.draw(enemy.sprite, enemy.x, enemy.y)
 end
-
--- function love.keypressed( key )
---     if love.keyboard.isDown("up") and playerCanJump then
---       player.yvel = player.jumpVel
---       playerCanJump = false
---     end
---   end
-
-
-
-
-
-
-
-
-
--- codigo do caio
-
---   function checkCollision(a, b)
---     -- não-colisão no eixo x
---     if
---       b.x > a.x + a.width or
---       a.x > b.x + b.width then
-
---       return false
---     end
-
---     -- não-colisão no eixo y
---     if
---       b.y > a.y + a.height or
---       a.y > b.y + b.height then
-
---       return false
---     end
-
---     return true
---   end
-
---
-
---   function love.keypressed(key)
---     if key == "escape" then
---       love.event.quit()
---     end
---   end
-
---   function love.update()
---     -- player
---     if love.keyboard.isDown("left") then
---       player.x = player.x - player.velx
---     end
-
---     if love.keyboard.isDown("right") then
---       player.x = player.x + player.velx
---     end
-
---     if player.x < 0 then
---       player.x = 0
---     end
-
---     if player.x + player.width > game.width then
---       player.x = game.width - player.width
---     end
-
---     -- bola
---     ball.vely = ball.vely + 0.1
-
---     ball.x = ball.x + ball.velx
---     ball.y = ball.y + ball.vely
-
---     if
---       checkCollision(ball, player) or
---       ball.y + ball.height > game.height then
-
---       ball.vely = -7
---       hitSound:play()
---     end
-
---     if
---       ball.x < 0 or
---       ball.x + ball.width > game.width then
-
---       ball.velx = ball.velx * -1
---       hitSound:play()
---     end
---   end
-
---   function love.draw()
---     love.graphics.scale(game.scale, game.scale)
-
---     -- definimos a cor azul
---     love.graphics.setColor(90/255, 215/255, 250/255)
---     love.graphics.rectangle("fill", 0, 0, game.width, game.height)
-
---     love.graphics.setColor(255/255, 255/255, 255/255)
---     -- love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
---     love.graphics.draw(player.image, player.x, player.y)
---     love.graphics.draw(ball.image, ball.x, ball.y)
---   end
